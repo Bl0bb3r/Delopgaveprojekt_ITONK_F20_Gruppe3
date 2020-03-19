6650f4169c5f928d49065b32dd8e665e5129f9a7
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2020-03-19 12:53:47 +0100

    Add project files.

---
+++ Delopgaveprojekt_ITONK_F20_Gruppe3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Delopgaveprojekt_ITONK_F20_Gruppe3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
