67c06646259bdcec2c41f3bef41edfbc101008c3
Mikkel Bleeg Carstensen <mikkelbleeg@hotmail.com>
2020-03-20 18:54:39 +0100

    Made changes in Database factory (Added Nuget for it to work)

--- Delopgaveprojekt_ITONK_F20_Gruppe3/AppDB/DatabaseFactory.cs
+++ Delopgaveprojekt_ITONK_F20_Gruppe3/AppDB/DatabaseFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
@@ -10,13 +11,13 @@ namespace Delopgaveprojekt_ITONK_F20_Gruppe3.AppDB
 {
     public class DatabaseFactory : IDatabaseFactory
     {
-        private SqlConnection con = new SqlConnection(Constants.sqlConnectionString);
+        private SqlConnection con = new SqlConnection(Constants.sqlConnectionString); //Missing connection string in constants folder & missing constants folder
 
         public IDatabase GetDatabase()
         {
-            connection.Open();
+            con.Open();
 
-            return new Database(connection);
+            return new Database(con);
         }
     }
 
