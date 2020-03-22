a634a1220a075ca013df85e2aadf62d2ebe12ae8
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2020-03-22 18:20:25 +0100

    database factory removed

--- Delopgaveprojekt_ITONK_F20_Gruppe3/AppDB/IDatabaseFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
-
-namespace Delopgaveprojekt_ITONK_F20_Gruppe3.AppDB
-{
-    public interface IDatabaseFactory
-    {
-        IDatabase GetDatabase();
-    }
-}
