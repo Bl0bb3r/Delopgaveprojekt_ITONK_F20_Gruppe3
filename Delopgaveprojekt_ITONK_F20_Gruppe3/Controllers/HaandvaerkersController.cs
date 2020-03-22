284f9c533e153a89db75884204e9f6f9f6ee0cc9
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2020-03-22 18:37:18 +0100

    controllers and views created

---
+++ Delopgaveprojekt_ITONK_F20_Gruppe3/Controllers/HaandvaerkersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Delopgaveprojekt_ITONK_F20_Gruppe3.AppDbContext;
+using Delopgaveprojekt_ITONK_F20_Gruppe3.Models;
+
+namespace Delopgaveprojekt_ITONK_F20_Gruppe3.Controllers
+{
+    public class HaandvaerkersController : Controller
+    {
+        private readonly AppDbContext.AppDbContext _context;
+
+        public HaandvaerkersController(AppDbContext.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Haandvaerkers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Haandvaerkere.ToListAsync());
+        }
+
+        // GET: Haandvaerkers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var haandvaerker = await _context.Haandvaerkere
+                .FirstOrDefaultAsync(m => m.HaandvaerkerID == id);
+            if (haandvaerker == null)
+            {
+                return NotFound();
+            }
+
+            return View(haandvaerker);
+        }
+
+        // GET: Haandvaerkers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Haandvaerkers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HaandvaerkerID,HVFornavn,HVEfternavn,HVAnsaettelsedato,HVFagomraade")] Haandvaerker haandvaerker)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(haandvaerker);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(haandvaerker);
+        }
+
+        // GET: Haandvaerkers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var haandvaerker = await _context.Haandvaerkere.FindAsync(id);
+            if (haandvaerker == null)
+            {
+                return NotFound();
+            }
+            return View(haandvaerker);
+        }
+
+        // POST: Haandvaerkers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HaandvaerkerID,HVFornavn,HVEfternavn,HVAnsaettelsedato,HVFagomraade")] Haandvaerker haandvaerker)
+        {
+            if (id != haandvaerker.HaandvaerkerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(haandvaerker);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HaandvaerkerExists(haandvaerker.HaandvaerkerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(haandvaerker);
+        }
+
+        // GET: Haandvaerkers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var haandvaerker = await _context.Haandvaerkere
+                .FirstOrDefaultAsync(m => m.HaandvaerkerID == id);
+            if (haandvaerker == null)
+            {
+                return NotFound();
+            }
+
+            return View(haandvaerker);
+        }
+
+        // POST: Haandvaerkers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var haandvaerker = await _context.Haandvaerkere.FindAsync(id);
+            _context.Haandvaerkere.Remove(haandvaerker);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HaandvaerkerExists(int id)
+        {
+            return _context.Haandvaerkere.Any(e => e.HaandvaerkerID == id);
+        }
+    }
+}
---
+++ Delopgaveprojekt_ITONK_F20_Gruppe3/Controllers/VaerktoejsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Delopgaveprojekt_ITONK_F20_Gruppe3.AppDbContext;
+using Delopgaveprojekt_ITONK_F20_Gruppe3.Models;
+
+namespace Delopgaveprojekt_ITONK_F20_Gruppe3.Controllers
+{
+    public class VaerktoejsController : Controller
+    {
+        private readonly AppDbContext.AppDbContext _context;
+
+        public VaerktoejsController(AppDbContext.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vaerktoejs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vaerktoejer.ToListAsync());
+        }
+
+        // GET: Vaerktoejs/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vaerktoej = await _context.Vaerktoejer
+                .FirstOrDefaultAsync(m => m.VTID == id);
+            if (vaerktoej == null)
+            {
+                return NotFound();
+            }
+
+            return View(vaerktoej);
+        }
+
+        // GET: Vaerktoejs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vaerktoejs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VTID,VTFabrikat,VTSerienr,VTModel,VTType,VTAnskaffet,LiggerIvtk")] Vaerktoej vaerktoej)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vaerktoej);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vaerktoej);
+        }
+
+        // GET: Vaerktoejs/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vaerktoej = await _context.Vaerktoejer.FindAsync(id);
+            if (vaerktoej == null)
+            {
+                return NotFound();
+            }
+            return View(vaerktoej);
+        }
+
+        // POST: Vaerktoejs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("VTID,VTFabrikat,VTSerienr,VTModel,VTType,VTAnskaffet,LiggerIvtk")] Vaerktoej vaerktoej)
+        {
+            if (id != vaerktoej.VTID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vaerktoej);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VaerktoejExists(vaerktoej.VTID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vaerktoej);
+        }
+
+        // GET: Vaerktoejs/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vaerktoej = await _context.Vaerktoejer
+                .FirstOrDefaultAsync(m => m.VTID == id);
+            if (vaerktoej == null)
+            {
+                return NotFound();
+            }
+
+            return View(vaerktoej);
+        }
+
+        // POST: Vaerktoejs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var vaerktoej = await _context.Vaerktoejer.FindAsync(id);
+            _context.Vaerktoejer.Remove(vaerktoej);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VaerktoejExists(long id)
+        {
+            return _context.Vaerktoejer.Any(e => e.VTID == id);
+        }
+    }
+}
